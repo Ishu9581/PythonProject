f8cce3689718c4730831fae891f4d0513bf40a8c
Ishu9581 <130961999+Ishu9581@users.noreply.github.com>
2023-05-08 10:44:52 +0530

    Update connection.py

--- connection.py
+++ connection.py
@@ -2,5 +2,5 @@ import mysql.connector
 mydb=mysql.connector.connect(host="localhost",user="root",password="Ishu@80742683")
 print(mydb.connection_id)
 cur=mydb.cursor()
-cur.execute('create database IR')
+cur.execute('create database Inventory_Management')
 
